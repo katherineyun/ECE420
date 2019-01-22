d04b69b3a0684c249d25c3d7d6a89d916336354d
katherineyun <32107625+katherineyun@users.noreply.github.com>
2019-01-21 21:27:59 -0600

    Uploaded files for lab1 and lab2

---
+++ lab2/app/src/main/cpp/ece420_main.cpp
@@ -0,0 +1,84 @@
+//
+// Created by daran on 1/12/2017 to be used in ECE420 Sp17 for the first time.
+// Modified by dwang49 on 1/1/2018 to adapt to Android 7.0 and Shield Tablet updates.
+//
+
+#include "ece420_main.h"
+
+// Student Variables
+#define FRAME_SIZE 128
+
+// FIR Filter Function Defined here located at the bottom
+int16_t firFilter(int16_t sample);
+
+void ece420ProcessFrame(sample_buf *dataBuf) {
+    // Keep in mind, we only have a small amount of time to process each buffer!
+    struct timeval start;
+    gettimeofday(&start, NULL);
+
+    // Using {} initializes all values in the array to zero
+    int16_t bufferIn[FRAME_SIZE] = {};
+    int16_t bufferOut[FRAME_SIZE] = {};
+
+    // Your buffer conversion (unpacking) here
+    // Fetch data sample from dataBuf->buf_[], unpack and put into bufferIn[]
+    // ******************** START YOUR CODE HERE ******************** //
+
+
+
+    // ********************* END YOUR CODE HERE ********************* //
+
+    // Loop code provided as a suggestion. This loop simulates sample-by-sample processing.
+    for (int sampleIdx = 0; sampleIdx < FRAME_SIZE; sampleIdx++) {
+        // Grab one sample from bufferIn[]
+        int16_t sample = bufferIn[sampleIdx];
+        // Call your filFilter funcion
+        int16_t output = firFilter(sample);
+        // Grab result and put into bufferOut[]
+        bufferOut[sampleIdx] = output;
+    }
+
+    // Your buffer conversion (packing) here
+    // Fetch data sample from bufferOut[], pack them and put back into dataBuf->buf_[]
+    // ******************** START YOUR CODE HERE ******************** //
+
+
+
+    // ********************* END YOUR CODE HERE ********************* //
+
+	// Log the processing time to Android Monitor or Logcat window at the bottom
+    struct timeval end;
+    gettimeofday(&end, NULL);
+    LOGD("Loop timer: %ld us",  ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)));
+
+}
+
+// TODO: Change N_TAPS to match your filter design
+#define N_TAPS 5
+// TODO: Change myfilter to contain the coefficients of your designed filter.
+double myfilter[N_TAPS] = {1,0,0,0,0};
+
+// Circular Buffer
+int16_t circBuf[N_TAPS] = {};
+int16_t circBufIdx = 0;
+
+// FirFilter Function
+int16_t firFilter(int16_t sample) {
+    // This function simulates sample-by-sample processing. Here you will
+    // implement an FIR filter such as:
+    //
+    // y[n] = a x[n] + b x[n-1] + c x[n-2] + ...
+    //
+    // You will maintain a circular buffer to store your prior samples
+    // x[n-1], x[n-2], ..., x[n-k]. Suggested initializations circBuf
+    // and circBufIdx are given.
+    //
+    // Input 'sample' is the current sample x[n].
+    // ******************** START YOUR CODE HERE ******************** //
+    int16_t output = 0;
+
+
+
+    // ********************* END YOUR CODE HERE ********************* //
+    return output;
+}
